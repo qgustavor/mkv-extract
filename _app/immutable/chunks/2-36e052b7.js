e088badb4745c5ea975bc63c639f9fd39b8a4834
qgustavor <qgustavor@users.noreply.github.com>
2023-01-31 15:21:24 +0000

    deploy: 544d1fec4d2f3e22ece18110b327ec26c5eb777f

---
+++ _app/immutable/chunks/2-36e052b7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-b47b3bf5.js";export{t as component};
--- _app/immutable/chunks/2-ded115a2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-1e63b842.js";export{t as component};
