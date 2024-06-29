b466ad9e02ccc591bb0561f64c67fef4469437ec
qgustavor <qgustavor@users.noreply.github.com>
2024-06-29 16:10:50 +0000

    deploy: 4d94e1c103e2e38d590486f5076908a87e460073

---
+++ _app/immutable/chunks/paths.DWZTEbDX.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_thlj6j)==null?void 0:s.base)??"/mkv-extract";var t;const e=((t=globalThis.__sveltekit_thlj6j)==null?void 0:t.assets)??a;export{e as a,a as b};
