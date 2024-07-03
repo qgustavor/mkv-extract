650895149db138c14277e531aa655b4a8791b20a
qgustavor <qgustavor@users.noreply.github.com>
2024-07-02 23:48:51 -0300

    Remove purgecss
    It was not working as expected.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { ViteToml } from 'vite-plugin-toml'
 import path from 'path'
 import type { UserConfig } from 'vite'
-import purgecss from '@erbelion/vite-plugin-sveltekit-purgecss'
 
 const config: UserConfig = {
-	plugins: [sveltekit(), ViteToml(), purgecss()],
+	plugins: [sveltekit(), ViteToml()],
   resolve: {
     alias: {
       '/~': path.resolve(__dirname, 'src')
